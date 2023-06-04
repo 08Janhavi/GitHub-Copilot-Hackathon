e984d880a9f6bbefcdfee12365509e8d50512144
08Janhavi <96649747+08Janhavi@users.noreply.github.com>
2023-06-04 22:03:31 +0530

    Update p.js

--- p.js
+++ p.js
@@ -1,129 +1,3 @@
-// const balance = document.querySelector("#balance");
-// const inc_amt = document.querySelector("#inc-amt");
-// const exp_amt = document.querySelector("#exp-amt");
-// const trans = document.querySelector("#trans");
-// const form = document.querySelector("#form");
-// const description = document.querySelector("#desc");
-// const amount = document.querySelector("#amount");
-// /*
-// const dummyData = [
-//   { id: 1, description: "Flower", amount: -20 },
-//   { id: 2, description: "Salary", amount: 35000 },
-//   { id: 3, description: "Book", amount: 10 },
-//   { id: 4, description: "Camera", amount: -150 },
-//   { id: 5, description: "Petrol", amount: -250 },
-// ];
-
-// let transactions = dummyData;
-// */
-
-// const localStorageTrans = JSON.parse(localStorage.getItem("trans"));
-// let transactions = localStorage.getItem("trans") !== null ? localStorageTrans : [];
-
-// function loadTransactionDetails(transaction) {
-//   const sign = transaction.amount < 0 ? "-" : "+";
-//   const item = document.createElement("li");
-//   item.classList.add(transaction.amount < 0 ? "exp" : "inc");
-//   item.innerHTML = `
-//     ${transaction.description}
-//     <span>${sign} ${Math.abs(transaction.amount)}</span>
-//     <span class="category">${transaction.category}</span>
-//     <button class="btn-del" onclick="removeTrans(${transaction.id})">x</button>
-//     <button class="btn-edit" onclick="editTrans(${transaction.id})">Edit</button>
-//   `;
-//   trans.appendChild(item);
-// }
-
-
-
-// function removeTrans(id) {
-//   if (confirm("Are you sure you want to delete Transcation?")) {
-//     transactions = transactions.filter((transaction) => transaction.id != id);
-//     config();
-//     updateLocalStorage();
-//   } else {
-//     return;
-//   }
-// }
-
-// function updateAmount() {
-//   const amounts = transactions.map((transaction) => transaction.amount);
-//   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-//   balance.innerHTML = `₹  ${total}`;
-
-//   const income = amounts
-//     .filter((item) => item > 0)
-//     .reduce((acc, item) => (acc += item), 0)
-//     .toFixed(2);
-//   inc_amt.innerHTML = `₹  ${income}`;
-
-//   const expense = amounts
-//     .filter((item) => item < 0)
-//     .reduce((acc, item) => (acc += item), 0)
-//     .toFixed(2);
-//   exp_amt.innerHTML = `₹  ${Math.abs(expense)}`;
-// }
-// function config() {
-//   trans.innerHTML = "";
-//   transactions.forEach(loadTransactionDetails);
-//   updateAmount();
-// }
-
-// function addTransaction(e) {
-//   e.preventDefault();
-//   if (
-//     description.value.trim() == "" ||
-//     amount.value.trim() == "" ||
-//     category.value.trim() == ""
-//   ) {
-//     alert("Please enter description, amount, and category.");
-//   } else {
-//     const transaction = {
-//       id: uniqueId(),
-//       description: description.value,
-//       amount: +amount.value,
-//       category: category.value,
-//     };
-//     transactions.push(transaction);
-//     loadTransactionDetails(transaction);
-//     description.value = "";
-//     amount.value = "";
-//     category.value = "";
-//     updateAmount();
-//     updateLocalStorage();
-//   }
-// }
-
-
-// function uniqueId() {
-//   return Math.floor(Math.random() * 10000000);
-// }
-
-// form.addEventListener("submit", addTransaction);
-
-// window.addEventListener("load", function () {
-//   config();
-// });
-
-// function updateLocalStorage() {
-//   localStorage.setItem("trans", JSON.stringify(transactions));
-// }
-
-// function editTrans(id) {
-//   const transaction = transactions.find((trans) => trans.id === id);
-//   if (transaction) {
-//     const updatedDescription = prompt("Enter a new description:", transaction.description);
-//     const updatedAmount = prompt("Enter a new amount:", transaction.amount);
-//     if (updatedDescription && updatedAmount) {
-//       transaction.description = updatedDescription;
-//       transaction.amount = +updatedAmount;
-//       config();
-//       updateLocalStorage();
-//     }
-//   }
-// }
-
-
 const balance = document.querySelector("#balance");
 const inc_amt = document.querySelector("#inc-amt");
 const exp_amt = document.querySelector("#exp-amt");
@@ -261,34 +135,30 @@ function editTrans(id) {
     }
   }
 }
+// Calculate the total balance
+const totalBalance = Math.abs(transactions.reduce((acc, transaction) => acc + transaction.amount, 0));
 
-// Create an object to store the amounts for each description and category
-const amountsByDescription = transactions.reduce((acc, transaction) => {
-  const { description, amount, category } = transaction;
-  if (!acc[description]) {
-    acc[description] = { amount: 0, category };
+// Calculate the percentages for each category
+const percentages = transactions.reduce((acc, transaction) => {
+  const category = transaction.category.toLowerCase();
+  if (!acc[category]) {
+    acc[category] = 0;
   }
-  acc[description].amount += Math.abs(amount);
+  acc[category] += Math.abs(transaction.amount);
   return acc;
 }, {});
 
-// Calculate the total balance
-const totalBalance = Object.values(amountsByDescription).reduce((acc, { amount }) => acc + amount, 0);
+// Calculate the percentage values and colors
+const labels = [];
+const percentagesData = [];
+const colors = [];
 
-// Calculate the percentages for each description and category
-const percentagesByDescription = {};
-for (const description in amountsByDescription) {
-  const { amount, category } = amountsByDescription[description];
-  const percentage = ((amount / totalBalance) * 100).toFixed(2);
-  percentagesByDescription[description] = { percentage, category };
+for (const category in percentages) {
+  const percentage = ((percentages[category] / totalBalance) * 100).toFixed(2);
+  labels.push(category);
+  percentagesData.push(percentage);
+  colors.push(category === 'expense' ? 'rgba(255, 0, 0, 0.8)' : 'rgba(0, 128, 0, 0.8)');
 }
-
-// Prepare the data for the chart
-const labels = Object.keys(percentagesByDescription);
-const percentagesData = Object.values(percentagesByDescription).map(({ percentage }) => percentage);
-const colors = Object.values(percentagesByDescription).map(({ category }) =>
-  category.toLowerCase() === 'expense' ? 'rgba(255, 0, 0, 0.8)' : 'rgba(0, 128, 0, 0.8)'
-);
 
 // Create the spending pattern chart
 const ctx = document.getElementById('spending-chart').getContext('2d');
@@ -296,12 +166,10 @@ new Chart(ctx, {
   type: 'pie',
   data: {
     labels: labels,
-    datasets: [
-      {
-        data: percentagesData,
-        backgroundColor: colors,
-      },
-    ],
+    datasets: [{
+      data: percentagesData,
+      backgroundColor: colors,
+    }]
   },
   options: {
     responsive: true,
@@ -313,11 +181,10 @@ new Chart(ctx, {
         label: function (tooltipItem, data) {
           const label = data.labels[tooltipItem.index];
           const percentage = data.datasets[0].data[tooltipItem.index];
-          const category = percentagesByDescription[label].category;
-          return `${label} (${category}): ${percentage}%`;
-        },
-      },
-    },
+          return `${label}: ${percentage}%`;
+        }
+      }
+    }
   },
 });
 
